aae36cdd46c20f0b4c9cf6d125c9d2fa6a0905df
EZEN-14 <EZEN-14@EZEN-14-PC>
2018-06-12 19:03:29 +0900

    6/12

--- Chapter07/src/exam01/ex01/Problem_02.java
+++ Chapter07/src/exam01/ex01/Problem_02.java
@@ -12,14 +12,24 @@ public class Problem_02 {
 		int i;
 		
 		System.out.println("문자열을 입력");
-		str=s.nextLine();
+		str=s.nextLine();//ABcd12FFd89
 		
 		i=0;
 		do {
+			ch=str.charAt(i);
+			if(ch>='A' && ch<='Z' )//대문자 
+				upper_cnt++;
+				
+			if(ch>='a' && ch<='z')//소문자
+				lower_cnt++;
+			if(ch>='0' && ch<='9')//숫자
+				digit_cnt++;
 			
+			i++;
 			
 		}while(i<str.length());
-
+		System.out.printf("대문자 %d개, 소문자 %d개, 숫자 %d개\n",upper_cnt,lower_cnt,digit_cnt);
+		
 	}
 
 }
