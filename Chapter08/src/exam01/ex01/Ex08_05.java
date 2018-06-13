9e92df6f07f6fd94daad2143a2dd8392075075e0
EZEN-14 <EZEN-14@EZEN-14-PC>
2018-06-13 18:28:57 +0900

    6/13

---
+++ Chapter08/src/exam01/ex01/Ex08_05.java
@@ -0,0 +1,19 @@
+package exam01.ex01;
+
+public class Ex08_05 {
+
+	public static void main(String[] args) {
+		int[] aa=new int[100];
+		int bb[]=new int[100];
+		int i;
+		
+		for(i=0;i<100;i++) 
+			aa[i]=i*2;
+			i=99;
+			System.out.printf("%d\n",aa[i]);
+		
+			System.out.println(i);
+			
+	}
+
+}
