84715044db131634449089bc21d3ff491457c5ed
EZ-1401 <EZ-1401@EZ-1401-PC>
2018-06-21 19:09:14 +0900

    6/21

--- Chapter10/src/test10/ex01/Employer.java
+++ Chapter10/src/test10/ex01/Employer.java
@@ -1,5 +1,25 @@
-package test10.ex01;
+/*package test10.ex01;
 
 public class Employer {
-
+	private int myMoney;
+	
+	public Employer(int money) {
+		myMoney=money;
+		
+	}
+	
+	public void payForWork(Employee emp,int money) {
+		if(myMoney < money)
+			return;
+		emp.earnMoney(money);
+		myMoney-=money;
+		
+	}
+	
+	public void showMyMoney() {
+		System.out.println(myMoney);
+	}
+	
+	
 }
+*/
